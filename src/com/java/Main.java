be951a3308ff0f97bb482eff17203065df1fc1c5
Kabir Hossain <cabirxemine9@gmail.com>
2026-07-09 21:30:20 +0600

    init commit= scanner function

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -1,19 +1,10 @@
 package com.java;
-
+import java.util.Scanner;
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-
-        int [] marks = new int[3];
-        marks[0] = 97;
-        marks[1] = 100;
-        marks[2] = 45;
-
-        System.out.println(marks.length);
-        System.out.println(marks[1]);
-        Arrays.sort(marks);
-        System.out.println(marks[1]);
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your age:");
+        int age = sc.nextInt();
     }
 }
