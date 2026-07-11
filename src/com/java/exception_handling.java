c2515eb7bc68416f9773963ba9ecb3ca71c643a9
Kabir Hossain <cabirxemine9@gmail.com>
2026-07-11 21:06:59 +0600

    init commit= Exception handling

---
+++ src/com/java/exception_handling.java
@@ -0,0 +1,13 @@
+package com.java;
+
+public class exception_handling {
+    public static void main(String[] args) {
+        int [] marks = {12,14,15};
+        try{
+            System.out.println(marks[5]);
+        }catch(Exception exception){
+
+        }
+        System.out.println("his name is kabir");
+    }
+}
