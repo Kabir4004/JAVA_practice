7bbcedcfc6ea64cf04b2d9f00352d1d2a6f4616e
Kabir Hossain <cabirxemine9@gmail.com>
2026-07-12 19:58:27 +0600

    init commit= using class

---
+++ src/oop/Teacher.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Teacher {
+    String name, age;
+    String phone_number;
+
+    void display(){
+        System.out.println("name = "+name);
+        System.out.println("age= "+age);
+        System.out.println("phone_number= "+phone_number);
+    }
+}
---
+++ src/oop/test.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class test {
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher();
+        teacher1.name = "Kabir";
+        teacher1.age = "26";
+        teacher1.phone_number = "0161";
+
+        teacher1.display();
+    }
+}
