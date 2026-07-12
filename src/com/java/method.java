c9cc92f7ce373a28a4fa0750899eae0db2b1963c
Kabir Hossain <cabirxemine9@gmail.com>
2026-07-12 19:40:35 +0600

    init commit= method using class

--- src/com/java/method.java
+++ src/com/java/method.java
@@ -2,7 +2,9 @@ package com.java;
 
 import java.util.Scanner;
 public class method {
-    public static String input(){
+
+
+    /*public static String input(){
         Scanner input = new Scanner(System.in);
         System.out.println("enter your name:");
         String name = input.next();
@@ -15,5 +17,23 @@ public class method {
     public static void main(String[] args) {
             String name = input();
             display(name);
+    } */
+
+    static String name;
+
+    public static void input(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("what is your name:");
+        name = input.next();
     }
+
+    public static void display(){
+        System.out.println("hello, "+ name );
+    }
+
+    public static void main(String[] args){
+        input();
+        display();
+    }
+
 }
