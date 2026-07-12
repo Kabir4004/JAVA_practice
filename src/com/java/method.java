3eaadcc18536b8f07d69cf6a8038d4b1375d5968
Kabir Hossain <cabirxemine9@gmail.com>
2026-07-12 18:14:01 +0600

    init commit= method

--- src/com/java/method.java
+++ src/com/java/method.java
@@ -1,4 +1,19 @@
 package com.java;
 
+import java.util.Scanner;
 public class method {
+    public static String input(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your name:");
+        String name = input.next();
+        return name;
+    }
+    public static void display(String name){
+        System.out.println("Hello, "+ name + " how are you!");
+
+    }
+    public static void main(String[] args) {
+            String name = input();
+            display(name);
+    }
 }
