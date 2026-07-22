e56dd7deb9b17b638640bdfe7dbe36cc8ba93007
Kabir Hossain <cabirxemine9@gmail.com>
2026-07-22 07:13:21 +0600

    init commit= Singleton design pattern example

--- src/Singleton_pattern/DBconnection.java
+++ src/Singleton_pattern/DBconnection.java
@@ -2,20 +2,17 @@ package Singleton_pattern;
 
 public class DBconnection {
     private static DBconnection instance;
-
     private DBconnection(){
-        System.out.println("Database connected!");
+        System.out.println("DB connected");
     }
-
     public static DBconnection getInstance(){
         if(instance == null){
             instance = new DBconnection();
         }
         return instance;
     }
-
-    public void display(String query){
-        System.out.println("Executing:" + query);
+    public void display(String output){
+        System.out.println("hey: "+output);
     }
 }
 
--- src/Singleton_pattern/Main.java
+++ src/Singleton_pattern/Main.java
@@ -2,7 +2,7 @@ package Singleton_pattern;
 
 public class Main {
     public static void main(String[] args) {
-        DBconnection user1 = DBconnection.getInstance();
-        user1.display("hello");
+        DBconnection person1 = DBconnection.getInstance();
+        person1.display("hello");
     }
 }
